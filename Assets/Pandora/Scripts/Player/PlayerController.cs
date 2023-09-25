b41d4a81b2505e655e1576a10f2a85df4cbe0e84
NemoDouble <ehdud1228@naver.com>
2023-09-25 21:24:35 +0900

    Squashed commit of the following:
    commit 5776b91da0fde4ad1664428a1a033a86beb7a2cc
    Author: NemoDouble <ehdud1228@naver.com>
    Date:   Mon Sep 25 21:14:01 2023 +0900
    
        Feat : 플레이어 이동과 태그
    
    commit 62b14dd1e63f6276a2a2704ad60fd8a979556755
    Merge: 4a77105 2012869
    Author: NemoDouble <ehdud1228@naver.com>
    Date:   Tue Sep 19 16:12:07 2023 +0900
    
        Merge branch 'main' into player
    
    commit 4a77105db2ed905b0b66890fe00c0f72dde09656
    Author: NemoDouble <ehdud1228@naver.com>
    Date:   Tue Sep 19 15:59:50 2023 +0900
    
        Rename : Input Action 이름 변경

--- Assets/Pandora/Scripts/Player/PlayerController.cs
+++ Assets/Pandora/Scripts/Player/PlayerController.cs
@@ -1,19 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    // Components
+    private Rigidbody2D rb;
+    
+    // Tmp Status
+    public float tmpSpeed = 5f;
+    
+    // Variables
+    private Vector2 moveInput;
+    private bool isOnControl;
+    public bool onControlInit = true;
+    
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        isOnControl = onControlInit;
     }
 
     void Update()
     {
-        // new input system
-        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        Vector2 inputDir = input.normalized;
-        transform.Translate(inputDir * Time.deltaTime * 5f);
+        if(isOnControl)
+        {
+            // move by velocity
+            rb.velocity = moveInput * tmpSpeed;
+        }
+        else
+        {
+            // stop
+            rb.velocity = Vector2.zero;
+            // TODO : move by AI
+        }
+    }
+    
+    // input system event OnMove
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+    
+    // input system event OnTag
+    public void OnTag(InputValue value)
+    {
+        isOnControl = !isOnControl;
     }
 }
