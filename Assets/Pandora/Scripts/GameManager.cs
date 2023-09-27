7ddad2ec6b9f4f4c805bdc4e6b6e1b8c580291af
NemoDouble <ehdud1228@naver.com>
2023-09-27 20:01:33 +0900

    Feat : 데미지 이펙츠

---
+++ Assets/Pandora/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Pandora.Scripts
+{
+    public class GameManager : MonoBehaviour
+    {
+        // Singleton class
+        public static GameManager Instance { get; private set; }
+        
+        // Singleton Awake
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Debug.LogError("GameManager already exists!");
+                Destroy(this);
+            }
+        }
+        
+        // 숫차로 출력되는 데메지 이펙트 프리팹
+        // Insistate 후 Init() 호출하여 사용
+        public GameObject damageEffect;
+    }
+}
