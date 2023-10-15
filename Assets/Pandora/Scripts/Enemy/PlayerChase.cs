f4ef2b8f345ca4d202f39c0da235b3e826800f42
KHS <dnld1759@gmail.com>
2023-10-15 20:39:57 +0900

    Feat : 일반 몬스터 4종 prefab 생성
    Flying Eye, Mushroom, Skeleton, Goblin

--- Assets/Pandora/Scripts/Enemy/PlayerChase.cs
+++ Assets/Pandora/Scripts/Enemy/PlayerChase.cs
@@ -2,6 +2,7 @@ using Pandora.Scripts.Enemy;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class PlayerChase : MonoBehaviour
 {
@@ -26,6 +27,15 @@ public class PlayerChase : MonoBehaviour
             direction = target.transform.position - transform.parent.position;
             direction.Normalize();
             transform.parent.position += direction * speed * Time.deltaTime;
+            transform.parent.GetComponent<Animator>().SetFloat("Speed", direction.magnitude);
+            if( direction.x < 0)
+            {
+                transform.parent.GetComponent<SpriteRenderer>().flipX = true;
+            }
+            else
+            {
+                transform.parent.GetComponent<SpriteRenderer>().flipX = false;
+            }
         }
     }
 
