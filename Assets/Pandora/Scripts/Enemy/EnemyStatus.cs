9b51b9e3871ac530df78f43cebeb4548cde600b2
KHS <dnld1759@gmail.com>
2023-10-28 16:59:23 +0900

    Fix : 각 일반몬스터 AI를 MobAI로 통합
    유지보수성 향상을 위함

---
+++ Assets/Pandora/Scripts/Enemy/EnemyStatus.cs
@@ -0,0 +1,124 @@
+using Pandora.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class EnemyStatus
+{
+    private float _code;
+    private float _maxHealth;
+    private float _nowHealth;
+    private float _baseDamage;
+    private float _attackPower;
+    private float _defencePower;
+    private float _speed;
+    private float _attackSpeed;
+    private List<Buff> _buffs = new List<Buff>();
+    private List<Buff> _attackBuffs = new List<Buff>();
+
+    //------MOB CODE-----
+    //
+    // [Normal]
+    // 000 : FlyingEye
+    //
+    // [Melee]
+    // 100 : Goblin
+    // 101 : Mushroom
+    // 102 : Skeleton
+    //
+    //-------------------
+
+    public EnemyStatus(string mobName)
+    {
+        switch (mobName)
+        {
+            case "FlyingEye":
+                _code = 000; _maxHealth = 5; _nowHealth = 5; _baseDamage = 2; _attackPower = 1; _defencePower = 0; _speed = 1; _attackSpeed = 1;
+                break;
+            case "Goblin":
+                _code = 100; _maxHealth = 10; _nowHealth = 10; _baseDamage = 2; _attackPower = 2; _defencePower = 1; _speed = 1; _attackSpeed = 1;
+                break;
+            case "Mushroom":
+                _code = 101; _maxHealth = 15; _nowHealth = 15; _baseDamage = 3; _attackPower = 2; _defencePower = 2; _speed = 1; _attackSpeed = 1;
+                break;
+            case "Skeleton":
+                _code = 102; _maxHealth = 20; _nowHealth = 20; _baseDamage = 4; _attackPower = 2; _defencePower = 3; _speed = 1; _attackSpeed = 1;
+                break;
+        }
+    }
+    // 최대 체력
+    public float MaxHealth
+    {
+        get => _maxHealth + _buffs.Sum(buff => buff.MaxHealthChange);
+        set => _maxHealth = value;
+    }
+
+    public float NowHealth
+    {
+        get => _nowHealth + _buffs.Sum(buff => buff.NowHealthChange);
+        set => _nowHealth = value;
+    }
+
+    public float BaseDamage
+    {
+        get => _baseDamage + _buffs.Sum(buff => buff.BaseDamageChange);
+        set => _baseDamage = value;
+    }
+
+    /// <summary>
+    /// 공격력 기본데메지에 곱적용 됨 (1.0f = 100%)
+    /// </summary>
+    public float AttackPower
+    {
+        get => _attackPower + _buffs.Sum(buff => buff.AttackPowerChange);
+        set => _attackPower = value;
+    }
+
+    /// <summary>
+    /// 방어율 (0~1) ex)0.5 = 50%피해
+    /// </summary>
+    public float DefencePower
+    {
+        get => _defencePower + _buffs.Sum(buff => buff.DefencePowerChange);
+        set => _defencePower = value;
+    }
+
+    public float Speed
+    {
+        get => _speed + _buffs.Sum(buff => buff.SpeedChange);
+        set => _speed = value;
+    }
+
+    public float AttackSpeed
+    {
+        get => _attackSpeed + _buffs.Sum(buff => buff.AttackSpeedChange);
+        set => _attackSpeed = value;
+    }
+
+    public void AddBuffs(List<Buff> buffs)
+    {
+        if (buffs == null) return;
+        _buffs.AddRange(buffs);
+    }
+
+    public void RemoveBuff(Buff buff)
+    {
+        _buffs.Remove(buff);
+    }
+
+    public void AddAttackBuffs(List<Buff> buffs)
+    {
+        _attackBuffs.AddRange(buffs);
+    }
+
+    public void RemoveAttackBuff(Buff buff)
+    {
+        _attackBuffs.Remove(buff);
+    }
+
+    public List<Buff> GetAttackBuffs()
+    {
+        return _attackBuffs;
+    }
+}
--- Assets/Pandora/Scripts/Enemy/Mob/MobAI.cs
+++ Assets/Pandora/Scripts/Enemy/Mob/MobAI.cs
@@ -1,22 +1,30 @@
+using Pandora.Scripts.Enemy;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoblinAI : MonoBehaviour
+public class MobAI : MonoBehaviour
 {
     // Components
-    public float speed = 1f; //임시
     private Vector3 direction;
     private GameObject target;
-    public float attackRange = 2f; //임시
+    private string parentName;
+
+    //Status
+    public float speed = 1.0f; //임시
 
     private float timer;
     private int waitingTime;
+
+    public float attackRange = 1f; //임시
+    Vector3 attackRangePos;
 
     private void Start()
     {
         timer = 0.0f;
         waitingTime = 2;
+        parentName = transform.parent.name;
+        attackRangePos = GameObject.Find(parentName).transform.Find("AttackRange").transform.localPosition;
     }
 
     private void Update()
@@ -38,29 +46,37 @@ public class GoblinAI : MonoBehaviour
         if (target != null)
             distance = Vector2.Distance(transform.parent.position, target.transform.position);
 
+        //대기시간이 아닐 경우
         if (timer > waitingTime && target == collision.gameObject)
         {
+            direction = target.transform.position - transform.parent.position;
+            direction.Normalize();
+
+            //방향 전환
+            if (direction.x < 0)
+            {
+                transform.parent.GetComponent<SpriteRenderer>().flipX = true;
+                GameObject.Find(parentName).transform.Find("AttackRange").transform.localPosition = new Vector3(-attackRangePos.x, attackRangePos.y, 0);
+            }
+            else
+            {
+                transform.parent.GetComponent<SpriteRenderer>().flipX = false;
+                GameObject.Find(parentName).transform.Find("AttackRange").transform.localPosition = new Vector3(attackRangePos.x, attackRangePos.y, 0);
+            }
+
             //공격 사정거리밖이면 범위 내의 플레이어를 추적
             if (distance > attackRange)
             {
-                direction = target.transform.position - transform.parent.position;
-                direction.Normalize();
                 transform.parent.position += direction * speed * Time.deltaTime;
                 transform.parent.GetComponent<Animator>().SetFloat("Speed", direction.magnitude);
-
-                if (direction.x < 0)
-                    transform.parent.GetComponent<SpriteRenderer>().flipX = true;
-                else
-                    transform.parent.GetComponent<SpriteRenderer>().flipX = false;
-
             }
-            //공격 사정거리에 들어왔을 경우
+            //공격 사정거리에 들어왔을 경우 공격
             else
             {
-                transform.parent.GetComponent<Animator>().SetFloat("Speed", 0);
-                GameObject.Find("Goblin").transform.Find("AttackRange").gameObject.SetActive(true);
                 transform.parent.GetComponent<Animator>().SetTrigger("Attack");
+                transform.parent.GetComponent<Animator>().SetFloat("Speed", 0);
                 timer = 0;
+                GameObject.Find(parentName).transform.Find("AttackRange").gameObject.SetActive(true);
             }
         }
     }
