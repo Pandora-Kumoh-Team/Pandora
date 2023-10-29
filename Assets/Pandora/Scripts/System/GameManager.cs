a09b0a29518efeac9db38dd78546e8a6a4d08571
NemoDouble <ehdud1228@naver.com>
2023-10-29 20:38:06 +0900

    Feat : 인게임 UI prefab 관련 수정

--- Assets/Pandora/Scripts/System/GameManager.cs
+++ Assets/Pandora/Scripts/System/GameManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Pandora.Scripts.System
 {
@@ -19,16 +20,26 @@ namespace Pandora.Scripts.System
                 Debug.LogError("GameManager already exists!");
                 Destroy(this);
             }
+            
+            // 씬 로드시 할 행동
+            SceneManager.sceneLoaded += OnSceneLoaded;
         }
         
-        public GameObject player;
+        public Canvas inGameCanvas;
+        
         // 숫차로 출력되는 데메지 이펙트 프리팹
         // Insistate 후 Init() 호출하여 사용
         public GameObject damageEffect;
-
-        private void Start()
+        
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
         {
-            player = GameObject.FindGameObjectWithTag("Player");
+            // 캔버스 찾기
+            inGameCanvas = GameObject.Find("InGameCanvas").GetComponent<Canvas>();
+        }
+        
+        public void ExitGame()
+        {
+            Application.Quit();
         }
     }
 }
