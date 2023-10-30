9d5820462cb0dd50d7df2f222020fb8f9e8188c1
NemoDouble <ehdud1228@naver.com>
2023-10-30 21:38:43 +0900

    Feat : InGameUI 게임 종료 버튼 기능

---
+++ Assets/Pandora/Scripts/UI/InGameCanvasManager.cs
@@ -0,0 +1,21 @@
+﻿using Pandora.Scripts.System;
+using UnityEngine;
+
+namespace Pandora.Scripts.UI
+{
+    public class InGameCanvasManager : MonoBehaviour
+    {
+        public void OnPause()
+        {
+            var pausePanel = transform.Find("PauseMenu").gameObject;
+            var isMenuActive = pausePanel.activeSelf;
+            pausePanel.SetActive(!isMenuActive);
+            Time.timeScale = isMenuActive ? 1 : 0;
+        }
+        
+        public void ExitGame()
+        {
+            GameManager.ExitGame();
+        }
+    }
+}
