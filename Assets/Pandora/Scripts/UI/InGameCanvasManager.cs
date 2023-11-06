ed251c37897d7dba8edb0062290ec6189c9a3dbd
NemoDouble <ehdud1228@naver.com>
2023-11-07 04:11:27 +0900

    Feat : 플레이어 사망 처리

--- Assets/Pandora/Scripts/UI/InGameCanvasManager.cs
+++ Assets/Pandora/Scripts/UI/InGameCanvasManager.cs
@@ -1,5 +1,6 @@
 ﻿using Pandora.Scripts.System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Pandora.Scripts.UI
 {
@@ -11,6 +12,12 @@ namespace Pandora.Scripts.UI
             var isMenuActive = pausePanel.activeSelf;
             pausePanel.SetActive(!isMenuActive);
             Time.timeScale = isMenuActive ? 1 : 0;
+        }
+        
+        public void ReStart()
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene("MainMenu");
         }
         
         public void ExitGame()
