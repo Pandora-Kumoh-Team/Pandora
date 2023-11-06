6388dc376bc4aa2cf61a99794ca3f5b2eefd5881
NemoDouble <ehdud1228@naver.com>
2023-11-07 00:12:21 +0900

    Feat : 플레이어 카메라 추적
    + 플레이어 render layer 수정

---
+++ Assets/Pandora/Scripts/Player/PlayerCameraPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Pandora.Scripts.Player.Controller;
+using UnityEngine;
+
+namespace Pandora.Scripts.Player
+{
+    public class PlayerCameraPoint : MonoBehaviour
+    {
+        private void Update()
+        {
+            var player1 = PlayerManager.Instance.transform.Find("PlayerCharacterMelee");
+            var player2 = PlayerManager.Instance.transform.Find("PlayerCharacterRanged");
+            
+            // set camera position to the middle of the two players
+            transform.position = (player1.position + player2.position) / 2;
+        }
+    }
+}
