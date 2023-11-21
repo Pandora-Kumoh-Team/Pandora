7def1860e630813c3fa93e2ce430f4b4fbc1e3a7
NemoDouble <ehdud1228@naver.com>
2023-11-21 18:32:40 +0900

    Feat : PlayerAI 구조 개선

---
+++ Assets/Pandora/Scripts/Player/AI/DeadState.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using NotImplementedException = System.NotImplementedException;
+
+namespace Pandora.Scripts.Player.Controller
+{
+    public class DeadState : PlayerAIState
+    {
+        public override void Enter(PlayerAI player)
+        {
+            player._playerController.canControllMove = false;
+        }
+
+        public override void Update(PlayerAI player)
+        {
+            player._playerController.playerCurrentStat.NowHealth +=
+                player._playerController.playerCurrentStat.NonControlHpRecovery * Time.deltaTime;
+            player._playerController.CallHealthChangedEvent();
+            if (player._playerController.playerCurrentStat.NowHealth >
+                player._playerController.playerCurrentStat.MaxHealth * 0.3f)
+            {
+                player._playerController.Rebirth();
+                player.ChangeState(new IdleState());
+            }
+        }
+
+        public override void Exit(PlayerAI player)
+        {
+            player._playerController.canControllMove = true;
+        }
+
+        public override void CheckTransition(PlayerAI player)
+        {
+            base.CheckTransition(player);
+        }
+    }
+}
---
+++ Assets/Pandora/Scripts/Player/AI/MoveToPointState.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using NotImplementedException = System.NotImplementedException;
+
+namespace Pandora.Scripts.Player.Controller
+{
+    public class MoveToPointState : PlayerAIState
+    {
+        private Vector2 _movePoint;
+
+        public override void Enter(PlayerAI player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(PlayerAI player)
+        {
+            // 이동 지점 도달시 다시 Idle 상태로
+            if (Vector2.Distance(player.transform.position, _movePoint) < 0.1f)
+            {
+                player.ChangeState(new IdleState());
+            }
+            else
+            {
+                player._playerController.moveDir = (_movePoint - (Vector2)player.transform.position).normalized;
+            }
+        }
+
+        public override void Exit(PlayerAI player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CheckTransition(PlayerAI player)
+        {
+            base.CheckTransition(player);
+        }
+    }
+}
