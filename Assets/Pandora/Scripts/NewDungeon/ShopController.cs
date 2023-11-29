ff2558ade44fc16b7da8f6f23fee0f3144ce495b
A-Yeong <dkdud064@kumoh.ac.kr>
2023-11-29 21:57:10 +0900

    Feat: 상점 UI 구현
    Feat: 상점 UI 구현
    
    지정된 장소에서 Left Shift를 누르면 아이템을 구매할 수 있는 상점 UI가 생성됨

--- Assets/Pandora/Scripts/NewDungeon/ShopController.cs
+++ Assets/Pandora/Scripts/NewDungeon/ShopController.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ShopController : MonoBehaviour
 {
     public RectTransform uiGroup;
-    GameObject enterPlayer;
+    public RectTransform text;
+    Player enterPlayer;
 
-    public void Enter(GameObject player)
+    public void Enter(Player player)
     {
         enterPlayer = player;
         uiGroup.anchoredPosition = Vector3.zero;
+        text.anchoredPosition = Vector3.down * 1200;
     }
 
     public void Exit()
     {
         uiGroup.anchoredPosition = Vector3.down * 1200;
+        text.anchoredPosition = Vector3.zero;
     }
 }
