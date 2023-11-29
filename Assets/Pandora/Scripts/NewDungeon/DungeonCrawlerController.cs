191af82c445b389f3241b00369a178a0e493332a
A-Yeong <dkdud064@kumoh.ac.kr>
2023-11-30 00:42:35 +0900

    Fix: 보스, 상점방 생성 버그 수정
    Fix: 보스, 상점방 생성 버그 수정
    
    맵 생성 시 보스, 상점 방이 시작방에 생성되는 오류 수정

--- Assets/Pandora/Scripts/NewDungeon/DungeonCrawlerController.cs
+++ Assets/Pandora/Scripts/NewDungeon/DungeonCrawlerController.cs
@@ -12,18 +12,19 @@ public enum Direction
 
 public class DungeonCrawlerController : MonoBehaviour
 {
-    public static List<Vector2Int> positionsVisited = new List<Vector2Int>();
+    public static List<Vector2Int> positionVisited = new List<Vector2Int>();
     private static readonly Dictionary<Direction, Vector2Int> directionMovementMap = new Dictionary<Direction, Vector2Int>
     {
         {Direction.up, Vector2Int.up},
         {Direction.left, Vector2Int.left},
         {Direction.down, Vector2Int.down},
-        {Direction.right, Vector2Int.right},
+        {Direction.right, Vector2Int.right}
     };
+
     public static List<Vector2Int> GenerateDungeon(DungeonGenerationData dungeonData)
     {
         List<DungeonCrawler> dungeonCrawlers = new List<DungeonCrawler>();
-        
+
         for(int i = 0; i < dungeonData.numberOfCrawlers; i++)
         {
             dungeonCrawlers.Add(new DungeonCrawler(Vector2Int.zero));
@@ -36,10 +37,10 @@ public class DungeonCrawlerController : MonoBehaviour
             foreach(DungeonCrawler dungeonCrawler in dungeonCrawlers)
             {
                 Vector2Int newPos = dungeonCrawler.Move(directionMovementMap);
-                positionsVisited.Add(newPos);
+                positionVisited.Add(newPos);
             }
         }
 
-        return positionsVisited;
+        return positionVisited;
     }
 }
