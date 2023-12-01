65175012d2584c52fe7118557db21e63bbc99488
NemoDouble <ehdud1228@naver.com>
2023-12-01 11:37:45 +0900

    Feat : 조작 키 수정
    영구 저장 기능 작동안함

---
+++ Assets/Pandora/Scripts/System/SettingManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Pandora.Scripts.System
+{
+    public class SettingManager : MonoBehaviour
+    {
+        public InputActionAsset inputActionAsset;
+        private string _keyBindingPath = "KeyBindings";
+
+        private void Awake()
+        {
+            _keyBindingPath = Application.persistentDataPath + "/KeyBindings.json";
+            LoadKeyBindings();
+        }
+
+        public void SaveKeyBindings()
+        {
+            var json = inputActionAsset.ToJson();
+            File.WriteAllText(_keyBindingPath, json);
+        }
+        
+        public void LoadKeyBindings()
+        {
+            if (File.Exists(_keyBindingPath))
+            {
+                var json = File.ReadAllText(_keyBindingPath);
+                inputActionAsset.LoadFromJson(json);
+            }
+        }
+    }
+}
