2108a12d2c14affa1f2176dae1db94d9579b041b
NemoDouble <ehdud1228@naver.com>
2023-12-04 15:24:06 +0900

    Feat : 플레이어AI 공격 이동 개선
    플레이어 AI가 전투중인 방 밖을 목표점으로 삼지 않도록 수정

---
+++ Assets/Pandora/Scripts/NewDungeon/Rooms/InRoomCollider.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Pandora.Scripts.Player.Controller;
+using UnityEngine;
+
+namespace Pandora.Scripts.NewDungeon.Rooms
+{
+    [RequireComponent(typeof(BoxCollider2D))]
+    public class InRoomCollider : MonoBehaviour
+    {
+        private Room _room;
+        private CompositeCollider2D _collider;
+        private List<PlayerController> _players;
+        
+        private void Awake()
+        {
+            _room = GetComponentInParent<Room>();
+            _collider = GetComponent<CompositeCollider2D>();
+            _players = new List<PlayerController>();
+        }
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                var pc = other.GetComponent<PlayerController>();
+                if (_players.Contains(pc)) return;
+                _players.Add(pc);
+                if (pc.onControl)
+                    _room.OnPlayerEnter(other.gameObject);
+                else
+                    pc.GetComponent<PlayerAI>()._roomCollider = _collider;
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if(other.CompareTag("Player"))
+            {
+                var pc = other.GetComponent<PlayerController>();
+                if (!_players.Contains(pc)) return;
+                _players.Remove(pc);
+                if (pc.onControl)
+                    _room.OnPlayerExit(other.gameObject);
+                else if (pc.GetComponent<PlayerAI>()._roomCollider == _collider)
+                    pc.GetComponent<PlayerAI>()._roomCollider = null;
+            }
+        }
+    }
+}
