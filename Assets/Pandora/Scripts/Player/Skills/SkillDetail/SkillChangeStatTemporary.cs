d3dac7d3fd5ef380f679daca155dd27f63504555
KHS <dnld1759@gmail.com>
2023-12-05 01:12:59 +0900

    Feat : 액티브 스킬 사건의 지평선 추가

--- Assets/Pandora/Scripts/Player/Skills/SkillDetail/SkillChangeStatTemporary.cs
+++ Assets/Pandora/Scripts/Player/Skills/SkillDetail/SkillChangeStatTemporary.cs
@@ -42,7 +42,7 @@ namespace Pandora.Scripts.Player.Skill.SkillDetail
         {
             transform.localPosition = Vector3.zero;
             var pc = ownerPlayer.GetComponent<PlayerController>();
-            if(pc.playerNumber == 0) // 근접일시 공격 사거리 효과 감소
+            if(pc.playerNumber == 0 && id == 13) // 스킬 무기의 달인 근접일시 공격 사거리 효과 감소
             {
                 temporaryStat.attackRange /= 3;
                 costStat.attackRange /= 3;
