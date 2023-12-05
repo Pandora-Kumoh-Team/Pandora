414958ed6d3d562acd653d5f6c75dee08eaf67c8
NemoDouble <ehdud1228@naver.com>
2023-12-05 21:52:01 +0900

    Feat : UI Esc종료 버그 수정

--- Assets/Pandora/Scripts/UI/Skill/SkillReplaceUi.cs
+++ Assets/Pandora/Scripts/UI/Skill/SkillReplaceUi.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Pandora.Scripts.Player.Controller;
 using Pandora.Scripts.Player.Skill;
+using Pandora.Scripts.System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -17,7 +18,10 @@ namespace Pandora.Scripts.UI
         {
             PlayerManager.Instance.GetPlayer(PlayerNum).GetComponent<PlayerController>()
                 .SetActiveSkill(changeSkill, skillIndex);
-            Time.timeScale = 1;
+            var ingameCanvas = GameManager.Instance.inGameCanvas.GetComponent<InGameCanvasManager>();
+            ingameCanvas.RemoveUIElement(transform.parent.gameObject);
+            ingameCanvas.RemoveUIElement(transform.parent.parent.gameObject);
+            ingameCanvas.cantPopUpByPauseElement.Remove(transform.parent.parent.gameObject);
         }
 
         public void Init(GameObject infoSkill, int playerNum, GameObject changeSkill)
