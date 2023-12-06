e7772b7862f0c27a332fb3228f2a07d3f0c38af9
NemoDouble <ehdud1228@naver.com>
2023-12-06 10:34:18 +0900

    Fix : 보스방 미생성 버그
    마지막 수정 후 테스트는 진행 중

--- Assets/Pandora/Scripts/NewDungeon/DungeonGenerator.cs
+++ Assets/Pandora/Scripts/NewDungeon/DungeonGenerator.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Pandora.Scripts.DebugConsole;
 using UnityEngine;
 
 namespace Pandora.Scripts.NewDungeon
@@ -18,8 +19,27 @@ namespace Pandora.Scripts.NewDungeon
             RoomController.Instance.EnqueueRoomToGeneration("Start", 0, 0);
             int roomIndex = 0;
             int shopIndex = Random.Range(1, 10);
+            int bossIndex = dungeonRoomPositions.Count - 1;
+            while (dungeonRoomPositions[bossIndex] == Vector2Int.zero)
+            {
+                bossIndex--;
+                if (bossIndex >= -1) continue;
+                RoomPositionsGenerator.stage = 0;
+                RoomPositionsGenerator.RoomPositions.Clear();
+                dungeonRoomPositions = RoomPositionsGenerator.GenerateRoomPositions(dungeonGenerationData);
+                SpawnRooms(roomPositions);
+                return;
+            }
             foreach (var roomPosition in roomPositions)
             {
+                if (roomPosition == dungeonRoomPositions[^1] && roomPosition == Vector2.zero)
+                {
+                    Debug.LogWarning("Boss room is zero (" + roomPosition.x + ", " + roomPosition.y + ")");
+                    if(DebugBossMapGenerateTest._instance != null)
+                    {
+                        DebugBossMapGenerateTest._instance.AddBossDenied("Boss room is zero (" + roomPosition.x + ", " + roomPosition.y + ")");
+                    }
+                }
                 if(roomPosition == dungeonRoomPositions[^1] && roomPosition != Vector2Int.zero)
                 {
                     RoomController.Instance.EnqueueRoomToGeneration("End", roomPosition.x, roomPosition.y);
